c708adff72f1531b8363a81520d4e1127c313749
Manideep Guru <manideepguru18@gmail.com>
2026-01-14 19:39:31 +0530

    utils updated

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@ import express from 'express';
 
 dotenv.config({path: './.env'});
 
-connectDB();
+connectDB()
+.then(() => {
+  const app = express();
+  const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((error) => {
+    console.error("Failed to start server:", error);
+});
---
+++ src/utils/Apierror.js
@@ -0,0 +1,23 @@
+class Apierror extends Error {
+  constructor(message="Something went wrong",
+    error =[],
+    stack="",
+     statusCode)
+    {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
+        this.error = error;
+        this.success = false;
+        
+        if(stack){
+            this.stack = stack;
+        }else{
+            Error.captureStackTrace(this,this.constructor);
+        }
+    }
+
+}
+
+export default Apierror;
